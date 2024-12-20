e044dd9e1a86d8558527ddf8713a50be24701711
Jad-Bel <jadthegamer06@gmail.com>
2024-12-20 03:09:04 +0100

    adding a new dish is done and display all dishes inside the table also done

--- public/bin/modifyPlate.php
+++ public/bin/modifyPlate.php
@@ -1,6 +1,40 @@
 <?php 
     require "/xampp/htdocs/LA-MICHELINE/public/bin/addPlate.php";
     require "/xampp/htdocs/LA-MICHELINE/public/bin/connect.php";
+
+$plate_name = "";
+$plate_description = "";
+$plate_categorie = "";
+$plate_menu = "";
+
+$errorMessage = "";
+$succesMessage = "";
+
+if ($_SERVER['REQUEST_METHOD'] == "GET") {}
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $plate_name = $_POST['plateName'];
+    $plate_description = $_POST['plateDescription'];
+    $plate_categorie = $_POST['plateCategory'];
+    $plate_menu = $_POST['plateMenu'];
+
+        if (empty($plate_name) || empty($plate_description) || empty($plate_categorie)) {
+            $errorMessage = "Plate name, description, categorie are required";
+        }
+
+        $sql = "INSERT INTO `plats`(`nom_plat`, `description`, `type`, `menu_id`) VALUES ('$plate_name','$plate_description','$plate_categorie','$plate_menu')";
+        $result = $connect->query($sql);
+
+        if (!$result) {
+            $errorMessage = "Invalid query: " . $connect->error;
+        }
+
+        $succesMessage = "Plate added successfully";
+        $plate_name = "";
+        $plate_description = "";
+        $plate_categorie = "";
+        $plate_menu = "";
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
